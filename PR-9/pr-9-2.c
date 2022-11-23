a6f14314702e7a869a24dca521dd8596a1e63a95
maulikkikani <114164076+maulikkikani@users.noreply.github.com>
2022-11-23 19:16:05 +0530

    11-23-2022

---
+++ PR-9/pr-9-2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+struct Marks {
+    int roll_no,i;
+    char name[30];
+    float chem_marks, maths_marks, phy_marks;
+};
+
+
+int main() {
+	
+    struct Marks marks[3];
+    int i;
+    
+	
+    for(i=0;i<3;i++)
+    {
+	
+		printf("Student %d\n",i+1);
+        printf("Enter roll no. :\n");
+        scanf("%d", &marks[i].roll_no);
+        printf("Enter name :\n");
+        scanf("%s",marks[i].name);
+        printf("Enter Chemistry marks :\n");
+        scanf("%f", &marks[i].chem_marks);
+        printf("Enter Maths marks :\n");
+        scanf("%f", &marks[i].maths_marks);
+        printf("Enter Physics marks :\n");
+        scanf("%f", &marks[i].phy_marks);
+    }
+    
+    
+	for(i=0; i<3; i++) {
+	    printf("Student %d\n",i+1);
+	    float percentage = (marks[i].chem_marks + marks[i].maths_marks + marks[i].phy_marks)/300.0*100;
+	    printf("Percentage : %f\n", percentage);
+	}
+  return 0;
+}
